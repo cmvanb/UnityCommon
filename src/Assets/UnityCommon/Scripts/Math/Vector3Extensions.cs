e2b3675ec58cc490f0a7394b4ba7bd64cbe5e516
Casper <cmvanb@gmail.com>
2018-01-30 17:16:12 +0100

    add SetX/Y/Z methods to Vector3Extensions

--- src/Assets/UnityCommon/Scripts/Math/Vector3Extensions.cs
+++ src/Assets/UnityCommon/Scripts/Math/Vector3Extensions.cs
@@ -4,6 +4,24 @@ namespace AltSrc.UnityCommon.Math
 {
     public static class Vector3Extensions
     {
+        public static Vector3 SetX(this Vector3 v, float x)
+        {
+            v.Set(x, v.y, v.z);
+            return v;
+        }
+
+        public static Vector3 SetY(this Vector3 v, float y)
+        {
+            v.Set(v.x, y, v.z);
+            return v;
+        }
+
+        public static Vector3 SetZ(this Vector3 v, float z)
+        {
+            v.Set(v.x, v.y, z);
+            return v;
+        }
+
         public static Vector2 ToVec2XY(this Vector3 v)
         {
             return new Vector2(v.x, v.y);
